a69b2592fcf45a4568faa077267f84b28d994ef3
rumeysataas <43634279+rumeysataas@users.noreply.github.com>
2021-02-11 15:12:25 +0300

    Günel

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,28 +10,37 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+           // ProductMethod();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
 
+        private static void ProductMethod()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
             Console.WriteLine("************************Tüm ürün isimleri listelendi.*************************");
             foreach (var product in productManager.GetAll())
             {
-             
+
                 Console.WriteLine(product.ProductName);
-               
+
             }
             Console.WriteLine("*********************Category Id ye göre listelendi.********************");
             foreach (var product in productManager.GetAllByCategoryId(5))
             {
-                
+
                 Console.WriteLine(product.ProductName);
-               
+
             }
             Console.WriteLine("********************Fiyat aralığına göre listelendi.*******************");
-            foreach (var product in productManager.GetByUnitPrice(3,6))
+            foreach (var product in productManager.GetByUnitPrice(3, 6))
             {
-               
+
                 Console.WriteLine(product.ProductName);
-                
+
             }
         }
     }
